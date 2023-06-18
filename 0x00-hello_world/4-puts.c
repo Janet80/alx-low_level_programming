23661ee38d0bf7f6590c62caa559ec5e3e9fc5dd
Janet80 <asabeajanet90@gmail.com>
2023-06-18 08:53:44 -0700

    first c-programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the string "Programming is like building a
+ *              multilingual puzzle" using the puts function.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char message[] = "Programming is like building a multilingual puzzle";
+
+	puts(message);
+
+	return (0);
+}
+
