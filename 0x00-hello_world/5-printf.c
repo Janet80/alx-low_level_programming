911544f6088a649e9ace239d6c4f3195bc93d7c4
Janet80 <asabeajanet90@gmail.com>
2023-06-18 09:09:28 -0700

    piece of art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a piece of art using proper grammar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Art,\n");
+	printf("like poetry,\n");
+	printf("can stir the soul and ignite the imagination.\n");
+	printf("It transcends boundaries,\n");
+	printf("melding colors and shapes,\n");
+	printf("giving form to the formless,\n");
+	printf("and expressing the inexpressible.\n");
+	printf("Each stroke of the brush,\n");
+	printf("each line on the canvas,\n");
+	printf("speaks volumes,\n");
+	printf("whispering secrets of the artist's heart.\n");
+	printf("So let us marvel at this masterpiece,\n");
+	printf("a symphony of visual delights,\n");
+	printf("and embrace the wonders it unveils.\n");
+
+	return (0);
+}
+
