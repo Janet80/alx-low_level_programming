2663a4619dc821c7229456380b4b0e49fc935e02
Janet80 <asabeajanet90@gmail.com>
2023-06-18 09:01:02 -0700

    first c-programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char message[] = "Programming is like building a multilingual puzzle";
 
-	puts(Programming is like building a multilingual puzzle);
+	puts(message);
 
 	return (0);
 }
